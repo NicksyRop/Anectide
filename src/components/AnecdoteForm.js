e95078cec7a0243edd9d8b9817fd513bee1b0a64
NicksyRop <nicksonkipkorir25@gmail.com>
2022-02-11 11:32:34 +0300

    separated the Form and List into different components

---
+++ src/components/AnecdoteForm.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { createAnectode } from "../reducers/anecdoteReducer";
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch();
+  const addAnectode = (e) => {
+    e.preventDefault();
+    const tode = e.target.anectode.value;
+
+    e.target.anectode.value = "";
+
+    dispatch(createAnectode(tode));
+  };
+  return (
+    <div>
+      {" "}
+      <h2>create new</h2>
+      <form onSubmit={addAnectode}>
+        <div>
+          <input name="anectode" />
+        </div>
+        <button>create</button>
+      </form>
+    </div>
+  );
+};
+
+export default AnecdoteForm;
